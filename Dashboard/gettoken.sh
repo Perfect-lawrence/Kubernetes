15e06cf290cd58bdf3e691cd0a339b8a4180b5e3
xiangxiaohai@b1024.cn <xiangxiaohai@b1024.cn>
2019-09-11 09:48:12 +0800

    modified2

--- Dashboard/gettoken.sh
+++ Dashboard/gettoken.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+# https://kubernetes.io/docs/tasks/access-application-cluster/web-ui-dashboard/
 kubectl create -f .
-
+# 
 kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep kubernetes-dashboard-admin-token | awk '{print $1}')
